7ee918be401f269e03a0e3ae47f59984273c5151
julia-lahtina <j.lakhtin@gmail.com>
2024-02-11 18:41:11 +0100

    add new component Star

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import './App.css';
 import {Button} from "./components/Button";
+import {Star} from "./components/Star";
 
 function App() {
     return (
         <div className="App">
+            <Rating/>
+            <Accordion/>
             <Button title={"ON"} isOn={true}/>
             <Button title={"OFF"} isOn={false}/>
         </div>
     );
+}
+
+function Rating() {
+    return(
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menü</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Star.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Star = () => {
+    return (
+        <div>star</div>
+    );
+};
