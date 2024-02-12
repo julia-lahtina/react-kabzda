ddb26fd69d07b932932bd39b8732bfea34d3e10e
julia-lahtina <j.lakhtin@gmail.com>
2024-02-12 20:58:53 +0100

    add onclick

--- src/components/UncontrolledRating.tsx
+++ src/components/UncontrolledRating.tsx
@@ -11,15 +11,15 @@ export const UncontrolledRating = (props: RatingPropsType) => {
     return (
         <div>
             <Star selected={value > 0}/>
-            <button>1</button>
+            <button onClick={() => {setValue(1)}}>1</button>
             <Star selected={value > 1}/>
-            <button>2</button>
+            <button onClick={() => {setValue(2)}}>2</button>
             <Star selected={value > 2}/>
-            <button>3</button>
+            <button onClick={() => {setValue(3)}}>3</button>
             <Star selected={value > 3}/>
-            <button>4</button>
+            <button onClick={() => {setValue(4)}}>4</button>
             <Star selected={value > 4}/>
-            <button>5</button>
+            <button onClick={() => {setValue(5)}}>5</button>
         </div>
     )
 
