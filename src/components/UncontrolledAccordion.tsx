a249c2f018e912aa5338875dbdabf3d84d5398ff
julia-lahtina <j.lakhtin@gmail.com>
2024-02-13 14:35:22 +0100

    change component

--- src/components/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion.tsx
@@ -13,7 +13,7 @@ export const UncontrolledAccordion = (props: AccordionPropsType) => {
 
     return (
         <div>
-            <AccordionTitle title={props.title}/> <button onClick={() => {setCollapsed(true)}}>TOGGLE</button>
+            <AccordionTitle title={props.title}/> <button onClick={() => {setCollapsed(!collapsed)}}>TOGGLE</button>
             {!collapsed && <AccordionBody/>}
         </div>
     )
