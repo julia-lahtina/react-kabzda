42cb86a263b65758c107909fcc3a8111f77f211a
julia-lahtina <j.lakhtin@gmail.com>
2024-02-13 14:54:49 +0100

    refactoring

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -6,9 +6,13 @@ type StarPropsType = {
 export const Star = (props: StarPropsType) => {
     console.log("Star rendering")
 
-    if (props.selected) {
-        return <span><b>star</b> </span>;
-    } else {
-        return <span>star </span>
-    }
+    return (
+        props.selected ? <span><b>star</b> </span> : <span>star </span>
+    )
+
+    /*    if (props.selected) {
+            return <span><b>star</b> </span>;
+        } else {
+            return <span>star </span>
+        }*/
 };
