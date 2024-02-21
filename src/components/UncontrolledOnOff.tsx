f6e6b96b96b05b5bc8351b56b359d3ace780a6d3
julia-lahtina <j.lakhtin@gmail.com>
2024-02-21 21:52:37 +0100

    add new component UncontrolledOnOff

---
+++ src/components/UncontrolledOnOff.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+
+export type OnOffPropsType = {
+    // on: boolean
+}
+
+export const UncontrolledOnOff = (props: OnOffPropsType) => {
+    console.log("OnOff rendering")
+
+    let [on, setOn] = useState(false) // hook
+
+    console.log("on: " + on)
+
+    const onStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: on ? "green" : "white"
+    };
+    const offStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "3px",
+        padding: "2px",
+        backgroundColor: !on ? "red" : "white"
+    };
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "50%",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: on ? "green" : "red"
+    };
+
+    return (
+        <div>
+            <div style={onStyle} onClick={() => {setOn(true)}}>On</div>
+            <div style={offStyle} onClick={() => {setOn(false)}}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    );
+};
