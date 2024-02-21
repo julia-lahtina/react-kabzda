cb2eba2572f1e367ee0f67ba79f1c8a4e51d6296
julia-lahtina <j.lakhtin@gmail.com>
2024-02-21 21:31:14 +0100

    add new props

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Title} from "./components/Title";
-import {Accordion} from "./components/Accordion";
-import {OnOff} from "./components/OnOff";
-import {UncontrolledAccordion} from "./components/UncontrolledAccordion";
-import {UncontrolledRating} from "./components/UncontrolledRating";
+import {Title} from './components/Title';
+import {Accordion} from './components/Accordion';
+import {OnOff} from './components/UncontrolledOnOff';
+import {UncontrolledAccordion} from './components/UncontrolledAccordion';
+import {UncontrolledRating} from './components/UncontrolledRating';
+import {Rating, RatingValuePropsType} from './components/Rating';
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValuePropsType>(0)
+    let [accordCollapsed, setAccordCollapsed] = useState<boolean>(false)
+
     return (
         <div className="App">
+
+            <Accordion title={'MENÜ'} collapsed={accordCollapsed} onClick={setAccordCollapsed}/>
+
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+
             {/*<Title title={"This is APP component"}/>
             <Rating value={3}/>*/}
-{/*            <Accordion title={"Menü"} collapsed={true}/>
+            {/*            <Accordion title={"Menü"} collapsed={true}/>
             <Accordion title={"Films"} collapsed={false}/>*/}
 
-            <UncontrolledAccordion title={"Version 1"}/>
-            <UncontrolledAccordion title={"Version 2"}/>
+            {/*            <UncontrolledAccordion title={"Version 1"}/>
+            <UncontrolledAccordion title={"Version 2"}/>*/}
 
-            <UncontrolledRating/>
+            {/*<UncontrolledRating/>*/}
 
-{/*            <Rating value={1}/>
+
+            {/*            <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
             <Rating value={5}/>*/}
 
-            <OnOff/>
-{/*            <OnOff/>
+            {/*            <OnOff/>*/}
+            {/*            <OnOff/>
             <OnOff/>
             <OnOff/>*/}
 
