3ff7536ad1a720ace92f41b416dae2015f540e8d
julia-lahtina <j.lakhtin@gmail.com>
2024-02-21 19:23:54 +0100

    refactoring

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 type StarPropsType = {
+    value: 1 | 2 | 3 | 4 | 5
     selected: boolean
-    onClick: () => void
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
 }
 export const Star = (props: StarPropsType) => {
     console.log("Star rendering")
     debugger
 
-    return (
-        props.selected ? <span><b>star</b> </span> : <span>star </span>
-    )
-
+    return <span onClick={() => {props.setValue(props.value)}}>{props.selected ? <b>star </b> : 'star '}</span>  //star пишем в кавычках, т.к. находимся внутри JS - если писать без
+    //кавычек, то для JS это как переменная и будет ошибка, т.к. такой переменной у нас нет
 };
