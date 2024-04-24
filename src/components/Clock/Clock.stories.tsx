4b87544087aec906987f04109dd8d2b460050e83
julia-lahtina <j.lakhtin@gmail.com>
2024-04-24 16:23:42 +0200

    lesson 24 - useEffect images

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -1,4 +1,5 @@
 import {Clock} from './Clock';
+import {useState} from 'react';
 
 
 export default {
@@ -16,5 +17,59 @@ export const BaseDigitalExample = () => {
 
     return (
         <Clock mode={'digital'}/>
+    )
+}
+
+export const ClockToggleExample = () => {
+
+    const [digState, setDigState] = useState(false)
+    const [analogState, setAnalogState] = useState(false)
+
+    const handleClickDigitalMode = () => {
+        setDigState(!digState)
+        setAnalogState(false)
+    }
+
+    const handleClickAnalogMode = () => {
+        setAnalogState(!analogState)
+        setDigState(false)
+    }
+
+    return (
+        <>
+            {digState && <Clock mode={'digital'}/>}
+            {analogState && <Clock mode={'analog'}/>}
+
+
+                <button
+                    style={{
+                        marginTop: '20px',
+                        backgroundColor: 'blue',
+                        border: 'none',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                        height: '30px',
+                        color: 'white',
+                        marginRight: '20px'
+                    }}
+                    onClick={handleClickDigitalMode}
+                >
+                    digital mode
+                </button>
+                <button
+                    style={{
+                        marginTop: '20px',
+                        backgroundColor: 'blue',
+                        border: 'none',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                        height: '30px',
+                        color: 'white'
+                    }}
+                    onClick={handleClickAnalogMode}
+                >
+                    analog mode
+                </button>
+        </>
     )
 }
